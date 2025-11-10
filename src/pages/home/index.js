05dcff92ab7bbb2500d45fb05cefa575322aa8ed
TrinhDinhPhuc <phuccoi996@gmail.com>
2025-11-10 22:18:50 +0100

    Update avatar to avt.png

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,7 +8,7 @@ import { Portfolio } from "../portfolio";
 import { Projects } from "../projects";
 import React from "react";
 import Typewriter from "typewriter-effect";
-import avatarImage from "../../assets/images/avatar.jpg";
+import avatarImage from "../avt.png";
 
 export const Home = () => {
   return (
